cee14f06f872155dff38eb6e42ad97034ad9fa26
TsimpDim <dimitristsimpitas@gmail.com>
2023-04-09 22:39:55 +0200

    Fix indentation in step component

--- voby/src/app/set/set.component.ts
+++ voby/src/app/set/set.component.ts
@@ -1,146 +1,118 @@
-  import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-  import { MatDialog } from '@angular/material/dialog';
-  import { ActivatedRoute, Router } from '@angular/router';
-  import { Subscription } from 'rxjs';
-  import { getCountryEmoji } from '../countries';
-  import { SetFormComponent } from '../set-form/set-form.component';
-  import { WordFormComponent } from '../word-form/word-form.component';
-  import { VobyService } from '../_services/voby.service';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { getCountryEmoji } from '../countries';
+import { SetFormComponent } from '../set-form/set-form.component';
+import { WordFormComponent } from '../word-form/word-form.component';
+import { VobyService } from '../_services/voby.service';
 
-  interface word {
-    id: number;
-    word: string;
-    translation: string;
-    examples: {text: string, translation: string, id: number}[];
-    general: string;
-    plural: string;
-    favorite: boolean;
-    related_words: any[];
-    created: string
+interface word {
+  id: number;
+  word: string;
+  translation: string;
+  examples: {text: string, translation: string, id: number}[];
+  general: string;
+  plural: string;
+  favorite: boolean;
+  related_words: any[];
+  created: string
+}
+
+@Component({
+  selector: 'voby-set',
+  templateUrl: './set.component.html',
+  styleUrls: ['./set.component.scss']
+})
+export class SetComponent implements OnInit {
+
+  id: number = -1;
+  @ViewChild('searchInput') searchInput: ElementRef | undefined;
+
+  selectedWord: word | undefined = undefined;
+  filteredWords: word[] = [];
+  paramsSubscription: Subscription | undefined;
+  set: any | undefined;
+  vclass: any | undefined;
+  loading = false;
+  showingFavorites = false;
+  allWords: any[] = [];
+  getCountryEmoji = getCountryEmoji;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    public dialog: MatDialog,
+    public voby: VobyService
+  ) {
+    const state = this.router.getCurrentNavigation()?.extras.state;
+    if (state) {
+      this.vclass = state['selectedClass'];
+      this.set = state['selectedSet'];
+      this.allWords = state['allWords'];
+
+      if (localStorage.getItem('sort') == 'date_asc') {
+        this.set.words.sort((a: any, b: any) => a.created > b.created)
+      } else {
+        this.set.words.sort((a: any, b: any) => a.created < b.created)
+      }
+
+      this.selectedWord = this.set.words[0];
+    }
   }
 
-  @Component({
-    selector: 'voby-set',
-    templateUrl: './set.component.html',
-    styleUrls: ['./set.component.scss']
-  })
-  export class SetComponent implements OnInit {
+  ngOnInit() {
+    this.paramsSubscription = this.route.params.subscribe(params => {
+      this.id = +params['id']; // (+) converts string 'id' to a number
+    });
 
-    id: number = -1;
-    @ViewChild('searchInput') searchInput: ElementRef | undefined;
-
-    selectedWord: word | undefined = undefined;
-    filteredWords: word[] = [];
-    paramsSubscription: Subscription | undefined;
-    set: any | undefined;
-    vclass: any | undefined;
-    loading = false;
-    showingFavorites = false;
-    allWords: any[] = [];
-    getCountryEmoji = getCountryEmoji;
-
-    constructor(
-      private route: ActivatedRoute,
-      private router: Router,
-      public dialog: MatDialog,
-      public voby: VobyService
-    ) {
-      const state = this.router.getCurrentNavigation()?.extras.state;
-      if (state) {
-        this.vclass = state['selectedClass'];
-        this.set = state['selectedSet'];
-        this.allWords = state['allWords'];
-
-        if (localStorage.getItem('sort') == 'date_asc') {
-          this.set.words.sort((a: any, b: any) => a.created > b.created)
-        } else {
-          this.set.words.sort((a: any, b: any) => a.created < b.created)
-        }
-
-        this.selectedWord = this.set.words[0];
-      }
+    if (!this.set) {
+      this.getSet(this.id);
+    } else {
+      this.search();
     }
+  }
 
-    ngOnInit() {
-      this.paramsSubscription = this.route.params.subscribe(params => {
-        this.id = +params['id']; // (+) converts string 'id' to a number
-      });
+  ngOnDestroy() {
+    this.paramsSubscription?.unsubscribe();
+  }
 
-      if (!this.set) {
-        this.getSet(this.id);
+  selectWord(id: number) {
+    this.selectedWord = this.set.words.find((o: any) => o.id === id);
+  }
+
+  openWordForm() {
+    const dialogRef = this.dialog.open(WordFormComponent, {
+      width: '30%',
+      data: {
+        setId: this.set.id,
+        allWords: this.allWords,
+        edit: false
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(data => {
+      if (data !== undefined && data.word && data.examples) {
+        data.examples.forEach((e: any) => data.word.examples.push(e));
+      }
+
+      if (localStorage.getItem('sort') === 'date_asc') {
+        this.set.words.push(data.word);
       } else {
-        this.search();
+        this.set.words.unshift(data.word);
       }
-    }
+      this.set.words_today += 1;
+      this.search();
+    });
+  }
 
-    ngOnDestroy() {
-      this.paramsSubscription?.unsubscribe();
-    }
-
-    selectWord(id: number) {
-      this.selectedWord = this.set.words.find((o: any) => o.id === id);
-    }
-
-    openWordForm() {
-      const dialogRef = this.dialog.open(WordFormComponent, {
-        width: '30%',
-        data: {
-          setId: this.set.id,
-          allWords: this.allWords,
-          edit: false
-        }
-      });
-
-      dialogRef.afterClosed().subscribe(data => {
-        if (data !== undefined && data.word && data.examples) {
-          data.examples.forEach((e: any) => data.word.examples.push(e));
-        }
-
-        if (localStorage.getItem('sort') === 'date_asc') {
-          this.set.words.push(data.word);
-        } else {
-          this.set.words.unshift(data.word);
-        }
-        this.set.words_today += 1;
-        this.search();
-      });
-    }
-
-    deleteSelectedWord() {
-      if (this.selectedWord) {
-        this.voby.deleteWord(this.selectedWord.id)
-        .subscribe({
-          next: () => {
-            this.set.words.splice(this.set.words.findIndex((w: any) => w.id === this.selectedWord?.id), 1);
-            this.selectedWord = undefined;
-            this.search();
-          },
-          error: () => {
-            this.loading = false;
-          },
-          complete: () => this.loading = false
-        })
-      }
-    }
-
-    sortDateDesc() {
-      localStorage.setItem('sort', 'date_desc')
-      this.getSet(this.id, 'date_desc');
-    }
-
-    sortDateAsc() {
-      localStorage.setItem('sort', 'date_asc')
-      this.getSet(this.id, 'date_asc');
-    }
-
-    getSet(id: number, sort = localStorage.getItem('sort') || 'date_desc') {
-      this.voby.getSet(id, sort)
+  deleteSelectedWord() {
+    if (this.selectedWord) {
+      this.voby.deleteWord(this.selectedWord.id)
       .subscribe({
-        next: (data: any) => {
-          this.set = data;
-          this.vclass = data.vclass_info
-          this.selectedWord = this.set.words[0];
-          this.getAllWordsOfClass(data.vclass);
+        next: () => {
+          this.set.words.splice(this.set.words.findIndex((w: any) => w.id === this.selectedWord?.id), 1);
+          this.selectedWord = undefined;
           this.search();
         },
         error: () => {
@@ -149,91 +121,119 @@
         complete: () => this.loading = false
       })
     }
+  }
 
-    getAllWordsOfClass(classId: number) {
-      this.voby.getAllWordsOfClass(classId)
-      .subscribe({
-        next: (data: any) => {
-          this.allWords = data;
-        },
-        error: () => {
-          this.loading = false;
-        },
-        complete: () => this.loading = false
-      })
-    }
+  sortDateDesc() {
+    localStorage.setItem('sort', 'date_desc')
+    this.getSet(this.id, 'date_desc');
+  }
 
-    toggleFavorite(id: number, favorite: boolean) {
-      this.voby.editWordFavorite(id, !favorite)
-      .subscribe({
-        next: () => {
-          this.set.words.find((w: word) => w.id === id).favorite = !favorite;
-        }
-      });
-    }
+  sortDateAsc() {
+    localStorage.setItem('sort', 'date_asc')
+    this.getSet(this.id, 'date_asc');
+  }
 
-    deleteSet() {
-      this.voby.deleteSet(this.id)
-      .subscribe({
-        next: () => {
-          this.router.navigate(['/']);
-        },
-        error: () => {
-          this.loading = false;
-        },
-        complete: () => this.loading = false
-      })
-    }
-
-    editSet() {
-      const dialogRef = this.dialog.open(SetFormComponent, {
-        width: '30%',
-        data: {
-          classId: this.vclass.id,
-          setId: this.id,
-          name: this.set.name
-        },
-      });
-
-      dialogRef.afterClosed().subscribe(res => {
-        this.set.name = res.name;
-      })
-    }
-
-    editWord() {
-      const dialogRef = this.dialog.open(WordFormComponent, {
-        width: '30%',
-        data: {
-          word: this.selectedWord,
-          allWords: this.allWords,
-          edit: true
-        },
-      });
-
-      dialogRef.afterClosed().subscribe(res => {
-        if (res) {
-          let word = this.set.words.find((w: any) => w.id === this.selectedWord?.id);
-          Object.assign(word, res);
-        }
-      })
-    }
-
-    deselectWord() {
-      this.selectedWord = undefined;
-    }
-
-    toggleShowFavorites() {
-      if (this.showingFavorites) {
+  getSet(id: number, sort = localStorage.getItem('sort') || 'date_desc') {
+    this.voby.getSet(id, sort)
+    .subscribe({
+      next: (data: any) => {
+        this.set = data;
+        this.vclass = data.vclass_info
+        this.selectedWord = this.set.words[0];
+        this.getAllWordsOfClass(data.vclass);
         this.search();
-        this.showingFavorites = false;
-      } else {
-        let newWords: word[] = [];
-        this.set.words.filter((w: word) => w.favorite === true).forEach((i: any) => newWords.push(i));
-        this.filteredWords.splice(0, this.filteredWords.length);
-        newWords.forEach(nW => {
-          this.filteredWords.push(nW);
-        });
-        this.showingFavorites = true;
+      },
+      error: () => {
+        this.loading = false;
+      },
+      complete: () => this.loading = false
+    })
+  }
+
+  getAllWordsOfClass(classId: number) {
+    this.voby.getAllWordsOfClass(classId)
+    .subscribe({
+      next: (data: any) => {
+        this.allWords = data;
+      },
+      error: () => {
+        this.loading = false;
+      },
+      complete: () => this.loading = false
+    })
+  }
+
+  toggleFavorite(id: number, favorite: boolean) {
+    this.voby.editWordFavorite(id, !favorite)
+    .subscribe({
+      next: () => {
+        this.set.words.find((w: word) => w.id === id).favorite = !favorite;
+      }
+    });
+  }
+
+  deleteSet() {
+    this.voby.deleteSet(this.id)
+    .subscribe({
+      next: () => {
+        this.router.navigate(['/']);
+      },
+      error: () => {
+        this.loading = false;
+      },
+      complete: () => this.loading = false
+    })
+  }
+
+  editSet() {
+    const dialogRef = this.dialog.open(SetFormComponent, {
+      width: '30%',
+      data: {
+        classId: this.vclass.id,
+        setId: this.id,
+        name: this.set.name
+      },
+    });
+
+    dialogRef.afterClosed().subscribe(res => {
+      this.set.name = res.name;
+    })
+  }
+
+  editWord() {
+    const dialogRef = this.dialog.open(WordFormComponent, {
+      width: '30%',
+      data: {
+        word: this.selectedWord,
+        allWords: this.allWords,
+        edit: true
+      },
+    });
+
+    dialogRef.afterClosed().subscribe(res => {
+      if (res) {
+        let word = this.set.words.find((w: any) => w.id === this.selectedWord?.id);
+        Object.assign(word, res);
+      }
+    })
+  }
+
+  deselectWord() {
+    this.selectedWord = undefined;
+  }
+
+  toggleShowFavorites() {
+    if (this.showingFavorites) {
+      this.search();
+      this.showingFavorites = false;
+    } else {
+      let newWords: word[] = [];
+      this.set.words.filter((w: word) => w.favorite === true).forEach((i: any) => newWords.push(i));
+      this.filteredWords.splice(0, this.filteredWords.length);
+      newWords.forEach(nW => {
+        this.filteredWords.push(nW);
+      });
+      this.showingFavorites = true;
     }
   }
 
