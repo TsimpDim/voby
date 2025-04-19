ab1698fc1f62b5a4483f6124ce75ce77dd3420cb
TsimpDim <dimitristsimpitas@gmail.com>
2025-04-19 17:43:34 +0200

    Bump to Angular16

--- voby/src/app/services/auth-guard.service.ts
+++ voby/src/app/services/auth-guard.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService  {
 
   constructor(
     public authService: AuthService,
